a56edfe5b7687765a1a6d203ac371dd72c13de4c
https://github.com/rmeytar/Instagram.git <https://github.com/rmeytar/Instagram.git>
2021-05-05 18:21:54 +0300

    react music machine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { Howl, Howler } from "howler";
+import "./Square.css";
+import audio1 from "./Audio/01.mp3";
+import audio2 from "./Audio/02.mp3";
+import audio3 from "./Audio/03.mp3";
+import audio4 from "./Audio/04.mp3";
+import audio5 from "./Audio/05.mp3";
+import audio6 from "./Audio/06.mp3";
+import audio7 from "./Audio/07.mp3";
+import audio8 from "./Audio/08.mp3";
+import audio9 from "./Audio/09.mp3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const audio = [
+  { sound: audio1, label: "Future Funk" },
+  { sound: audio2, label: "Stutter Break" },
+  { sound: audio3, label: "Bass Groove" },
+  { sound: audio4, label: "Guitar" },
+  { sound: audio5, label: "Stompy Slosh" },
+  { sound: audio6, label: "Tanggu" },
+  { sound: audio7, label: "Maze Politics" },
+  { sound: audio8, label: "Pas Groove" },
+  { sound: audio9, label: "Silent Star" },
+];
+
+class App extends Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {isTogglePlay: false}
+  // }
+
+  // handleClick = () => {
+  //   this.setState(state => ({
+  //     isTogglePlay: !state.isTogglePlay
+  //   }));
+  // }
+
+  SoundPlay = (src) => {
+    const sound = new Howl({ src, loop: true });
+    if (!sound.playing()) {
+      sound.play();
+    }
+    sound.playing() ? sound.stop() : sound.play();
+  };
+
+  RenderButtonAndSound = () => {
+    return audio.map((soundObj, index) => {
+      return (
+        <button className="square">
+          {soundObj.label}
+          <br />
+          <br />
+          <button className="play-button" key={index} onClick={() => this.SoundPlay(soundObj.sound)}>
+          Play
+          </button>
+        </button>
+      );
+    });
+  };
+
+  render() {
+    Howler.volume(1.0);
+    return (
+      <div>
+        <h2>
+          <center>LOOP MACHINE</center>
+        </h2>
+        {this.RenderButtonAndSound()}
+      </div>
+    );
+  }
 }
 
 export default App;
